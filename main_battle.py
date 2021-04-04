7a047d6976f84b2fee6a6c495d6f3624a1e70917
Giorgio Alefe <giorgioa2002@gmail.com>
2021-04-04 18:52:42 -0300

    Create version 1.0 of the battle

--- main_battle.py
+++ main_battle.py
@@ -1,5 +1,7 @@
 from func_color import print_wcolor
+from random import randint
 import os, sys
+
 
 class presentation:
     
@@ -40,18 +42,66 @@ class presentation:
 
     def start(self, name):
         print_wcolor(35, "=" * 30)
-        print_wcolor(36, "[ 1 ] INICIAR BATALHA")
-        input('>>> ')
+        while True:
+            try:
+                
+                print_wcolor(36, "[ 1 ] INICIAR BATALHA")
+                answers = int(input('>>> '))
+            except(ValueError):
+                os.system('cls')
+                print_wcolor(31, 'Option not available! Try again.')
+                continue
+            if answers != 1:
+                os.system('cls')
+                print_wcolor(31, 'Option not available! Try again.')
+            elif answers == 1:
+                mob_hp = self.mob_hp
+                mob_at = self.mob_strength * self.mob_stamina
+                mob_def = self.mob_defense
+                mob_reg = (self.mob_defense / 5) * self.mob_stamina
 
-    def battle(self):
-        mob_hp = self.mob_hp
-        mob_at = self.mob_strength * self.mob_stamina
-        mob_def = self.mob_defense
-        mob_reg = (self.mob_defense / 5) * self.mob_stamina
+                en_hp = self.enemy_hp
+                en_at = self.enemy_strength * self.enemy_stamina
+                en_def = self.enemy_defense
+                en_reg = (self.enemy_defense / 5) * self.enemy_stamina
 
-        en_hp = self.enemy_hp
-        en_at = self.enemy_strength * self.enemy_stamina
-        en_def = self.enemy_defense
-        en_reg = (self.enemy_defense / 5) * self.enemy_stamina
+                while True:
+                    try:
+                        print_wcolor(36, '[ 1 ] ATTACK\n[ 2 ] REGENERATE')
+                        action = int(input('>>> '))
+                    except(ValueError):
+                        os.system('cls')
+                        print_wcolor(31, 'Option not available! Try again.')
+                        continue
+                    if action < 1:
+                        os.system('cls')
+                        print_wcolor(31, 'Option not available! Try again.')
+                        continue
+                    elif action > 2:
+                        os.system('cls')
+                        print_wcolor(31, 'Option not available! Try again.')
+                        continue
 
-        
+                    # -----BATTLE----
+                    elif action == 1:
+                        tiredness_1 = randint(1 ,10)
+                        tiredness_2 = randint(1 ,10)
+                        mob_at = tiredness_1
+                        en_at = tiredness_2
+                        os.system('cls')
+                        en_hp = en_hp - mob_at
+                        print(f'Deu {mob_at} de dano. Hp inimigo: {int(en_hp)}')
+                        mob_hp = mob_hp - en_at
+                        print(f'Levou {en_at} de dano. Seu Hp: {int(mob_hp)}')
+
+                    elif action == 2:
+                        mob_hp += mob_reg
+                        if mob_hp > 100:
+                            mob_hp = 100
+                        en_hp += en_reg
+                        if en_hp > 100:
+                            en_hp = 100
+                        os.system('cls')
+                        print(f'Você regenerou {mob_reg} de Hp. Seu Hp: {int(mob_hp)}')
+                        print(f'Seu inimigo regenerou {en_reg} de Hp. Hp inimigo: {int(en_hp)}')
+           
