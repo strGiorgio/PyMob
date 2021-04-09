233e9a8186ab8aa52cdcf54ad9fbedf729aead09
Giorgio Alefe <giorgioa2002@gmail.com>
2021-04-09 19:32:34 -0300

    Translate to English 3/3, create 2 languages in help menu

--- main.py
+++ main.py
@@ -5,10 +5,32 @@ from time import sleep
 
 # funções
 def help():
-    rules = '''GERAÇÃO - Um mob será gerado com suas habilidade(defesa,força e estamina) aleatórias.\n\nBATALHA - A batalha consiste em 1 inimigo com defesa, força e estamina aleatórias, a força do hit se resume na força vezes a estamina, a regeneração é a defesa dividido por 5 vezes a estamina\n\nCANSAÇO - Todo rodada tem a chance de 1 a 10 de cansaço, ou seja, seu mob poderá ter um cansaço e dar menos dano mesmo a sua força sendo maior que do inimigo em questão.'''
+    pt_br = '''GERAÇÃO - Um mob será gerado com suas habilidade(defesa,força e estamina) aleatórias.\n\nBATALHA - A batalha consiste em 1 inimigo com defesa, força e estamina aleatórias, a força do hit se resume na força vezes a estamina, a regeneração é a defesa dividido por 5 vezes a estamina\n\nCANSAÇO - Todo rodada tem a chance de 1 a 10 de cansaço, ou seja, seu mob poderá ter um cansaço e dar menos dano mesmo a sua força sendo maior que do inimigo em questão.'''
+    en = '''GENERATION - A mob will be generated with your random abilities (defense, strength and stamina).\n\nBATTLE - The battle consists of 1 enemy with random defense, strength and stamina, the strength of the hit is summarized in the strength times the stamina, the regeneration is the defense divided by 5 times the stamina\n\nFATNESS - Every round has a chance of 1 to 10 of tiredness, that is, your mob may be tired and do less damage even though your strength is greater than that of the enemy in question.'''
+    print_wcolor(35, '=' * 155)
+    print_wcolor(33, en)
+    print_wcolor(35, '=' * 155)
 
-    print_wcolor(33, rules)
-    print_wcolor(35, '=' * 100)
+    while True:
+        try:
+            print_wcolor(36, '[ 1 ] TRANSLATE PT-BR\n[ 2 ] RETURN')
+            ans = int(input('>>> '))
+        except(ValueError):
+            os.system('cls')
+            print_wcolor(31, 'Option not available! Try again.')
+            continue
+        if ans == 1:
+            os.system('cls')
+            print_wcolor(35, '=' * 155)
+            print_wcolor(33, pt_br)
+            print_wcolor(35, '=' * 155)
+            continue
+        elif ans == 2:
+            os.system('cls')
+            break
+        else:
+            os.system('cls')
+            print_wcolor(31, 'Option not available! Try again.')
 
 # INICIO DO GAME
 os.system('cls')
